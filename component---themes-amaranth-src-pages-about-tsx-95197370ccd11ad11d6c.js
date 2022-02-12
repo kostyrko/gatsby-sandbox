4ddac53ba5a440840a4dd1e707fe1ede82aafb83
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-02-12 18:02:31 +0100

    Updates

---
+++ component---themes-amaranth-src-pages-about-tsx-95197370ccd11ad11d6c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_advanced=self.webpackChunkgatsby_starter_advanced||[]).push([[467],{3853:function(e,t,a){a.r(t);var n=a(7294),r=a(5186),c=a(4095),s=a(8077);t.default=function(){var e=(0,c.ZR)();return n.createElement(s.Z,null,n.createElement("div",{className:"about-container"},n.createElement(r.q,{title:"About | "+e.website.title})))}}}]);
+//# sourceMappingURL=component---themes-amaranth-src-pages-about-tsx-95197370ccd11ad11d6c.js.map
